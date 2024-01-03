4475c2455b311a45b8fecc9fba7b459fd77c37d2
Laura <lauraalspaugh2002@icloud.com>
2024-01-03 15:58:49 -0700

    timer is finished

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container-fluid">
     <section class="row">
-      <div class="col-12">
+      <div class="col-12 p-0">
 
         <img class="img-fluid" :src="images.imgUrl" alt="image url">
         <p class="author-title fs-4 text-light"> Image by: {{ images.author }}</p>
-        <p> {{ quotes.content }}</p>
+        <p class="content-title fs-4 text-light"> {{ quotes.content }}</p>
+        <p class="fs-1 text-white text-center timer-content" id="demo"></p>
       </div>
     </section>
   </div>
@@ -42,6 +43,11 @@ export default {
         Pop.error(error)
       }
     }
+    function myTimer() {
+      const date = new Date();
+      document.getElementById("demo").innerHTML = date.toLocaleTimeString();
+    }
+    setInterval(myTimer, 1000);
     return {
       images: computed(() => AppState.images),
       quotes: computed(() => AppState.quotes)
@@ -64,5 +70,17 @@ img {
   bottom: 20px;
   left: 60px;
 
+}
+
+.content-title {
+  position: absolute;
+  top: 130px;
+  left: 80px;
+}
+
+.timer-content {
+  position: absolute;
+  bottom: 80px;
+  left: 600px;
 }
 </style>
